2e19e8b8d9956956b83e2653ef5f535354e49f7f
Husen Lokhandwala <coder.husen@gmail.com>
2020-10-14 10:08:13 +0530

    updated and routing added

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 var express = require('express');
 var app = express();
-var dt = require('./crud.js');
-
-app.get('/', function(req, res) {
-    res.send('Hello World!' + dt.crud());
-
-});
-
-app.listen(5000);
-
-
-
-
-var http = require('http');
 var mysql = require('mysql');
-const express = require('express');
-
-const app = express();
+var dt = require('./crud.js');
 
 var con = mysql.createConnection({
     host : "95.217.158.21",
@@ -30,12 +15,18 @@ con.connect(function(err) {
     console.log("Connected!");
 });
 
+
+app.get('/' , (req, res) => {
+    res.send("Hello World!");
+});
+
 //Creating GET Router to fetch all the learner details from the MySQL Database
 app.get('/user' , function(req, res) {
     const sql = "Select * from user"
     con.query(sql, (err, rows) => {
-    if (!err)
-    res.send(rows);
+    if (!err){
+        res.send(rows);
+    }
     else
     console.log(err);
     })
@@ -70,15 +61,6 @@ app.put('/user-update' , function(req, res) {
     console.log(err);
     })
 });
-
-var server = http.createServer(function(req, res){
-    res.write('Hiello');
-    res.end();
-});
-
-server.listen(5000);
-
-console.log('Server is Running at port 5000');
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000.");
